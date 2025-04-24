edf7c23ffbb286d0ebbf5dc41498ccc416143b95
Elevated Garage <desantis.nick@rocketmail.com>
2025-04-24 08:37:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
