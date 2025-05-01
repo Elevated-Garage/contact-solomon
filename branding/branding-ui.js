786a94a62ea315e6e3629c0ddf42231dc2952517
ca-nickd <desantis.nick@rocketmail.com>
2025-04-30 18:35:32 -0700

    update for branding folder

---
+++ branding/branding-ui.js
@@ -0,0 +1,13 @@
+export const uiBranding = {
+  title: "Solomon",
+  subtitle: "Your Garage Design Assistant",
+  logoPath: "/public/logo.png",
+  watermarkImage: "/public/watermark.png",
+  primaryColor: "#B91B21",
+  accentColor: "#EB6200",
+  backgroundColor: "#ffffff",
+  bubbleColorBot: "#f6f6f6",
+  bubbleColorUser: "#ffece9",
+  watermarkOpacity: 0.05,
+  borderRadius: "18px"
+};
