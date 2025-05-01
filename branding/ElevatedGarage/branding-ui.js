7cabeaf71234ed5121baf3a35c9bca7ee6ca6ae4
Elevated Garage <desantis.nick@rocketmail.com>
2025-04-30 22:22:32 -0700

    Update branding-ui.js

--- branding/ElevatedGarage/branding-ui.js
+++ branding/ElevatedGarage/branding-ui.js
@@ -1,8 +1,8 @@
 export const uiBranding = {
   title: "Let’s Elevate Your Garage!",
   subtitle: "Your smart garage designer is standing by—let’s build your vision together.",
-  logoPath: "/public/logo.png",
-  watermarkImage: "public/branding/Elevated Garage Icon Final.png",
+  logoPath: "/branding/9.png",
+  watermarkImage: "/branding/Elevated Garage Icon Final.png",
   primaryColor: "#B91B21",
   accentColor: "#EB6200",
   backgroundColor: "#fafafa",
