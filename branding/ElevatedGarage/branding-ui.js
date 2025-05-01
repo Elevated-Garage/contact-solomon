2f728d26effaaaa492fce3e8edbfbbeca31dcb7d
Elevated Garage <desantis.nick@rocketmail.com>
2025-04-30 22:19:20 -0700

    Update branding-ui.js

--- branding/ElevatedGarage/branding-ui.js
+++ branding/ElevatedGarage/branding-ui.js
@@ -5,8 +5,8 @@ export const uiBranding = {
   watermarkImage: "public/branding/Elevated Garage Icon Final.png",
   primaryColor: "#B91B21",
   accentColor: "#EB6200",
-  backgroundColor: "#ffffff",
-  bubbleColorBot: "#f6f6f6",
+  backgroundColor: "#fafafa",
+  bubbleColorBot: "#fce7e7",
   bubbleColorUser: "#ffece9",
   watermarkOpacity: 0.05,
   borderRadius: "18px"
