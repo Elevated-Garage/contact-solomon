5b3182fbf24ed1be5057e018936401f6a9c2ae79
Elevated Garage <desantis.nick@rocketmail.com>
2025-05-03 02:44:40 -0700

    Update sessions.js

--- utils/sessions.js
+++ utils/sessions.js
@@ -4,6 +4,12 @@
 const userConversations = {};
 const userUploadedPhotos = {};
 const userIntakeOverrides = {};
+}
+
+function generateSessionId() {
+  return Math.random().toString(36).substring(2, 15);
+}
+
 
 /**
  * Returns or initializes a session entry.
